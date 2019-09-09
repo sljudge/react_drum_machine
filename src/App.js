e804f379d5b1657adb65a81a6fe1826895c01de1
Sam Judge <samuel.judge.92@gmail.com>
2019-09-09 13:27:34 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,87 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faPowerOff, faArrowAltCircleRight} from '@fortawesome/free-solid-svg-icons';
+import {Container, Row, Col, Button} from 'reactstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Pad extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      play: false
+    }
+    this.handleDrum = this.handleDrum.bind(this)
+  }
+
+  handleDrum(){
+    console.log('playing')
+    const sound = document.getElementById(this.props.drum.key);
+    sound.play();
+  }
+
+  render(){
+    return(
+      <div className='outer-drum'>
+        <audio id={this.props.drum.key} src={'/drums/'+this.props.drum.source+'.wav'}></audio>
+        <Button className='drum' color='secondary' onClick={() => this.handleDrum()}></Button>
+      </div>
+    )
+  }
+}
+
+class Controls extends React.Component{
+  render(){
+    return(
+      <div id='controls' className='col-4'>
+        <Row className='control-row'>
+          <Button id='power' color='danger'><FontAwesomeIcon icon={faPowerOff}/></Button>
+        </Row>
+        <Row className='control-row'>
+          <input id='volume' type='range' className='custom-range' min='0' max='100'/>
+        </Row>
+        <Row className='control-row'>
+          <Col className='set-col' xs='6'>
+            <Button id='next' color='warning'><FontAwesomeIcon icon={faArrowAltCircleRight}/></Button>
+          </Col>
+          <Col className='set-col' xs='6'>
+            <div id='set'>
+              <p>SET</p>
+            </div>
+          </Col>
+        </Row>
+      </div>
+    )
+  }
+}
+
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state ={
+      power:  true,
+      volume: 50
+    }
+  };
+
+
+  render(){
+    const pads = this.props.drums.map((drum, i) => {
+      return(
+        <Pad key={i} drum={drum}/>
+      )
+    })
+
+    return(
+      <Container>
+        <Row id='shell'>
+            <Controls/>
+          <Col id='pads' xs='8' className='side'>
+            {pads}
+          </Col>
+        </Row>
+      </Container>
+    )
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+const data = [
+  {key:'q', keycode:81, source: 'kick1'},
+  {key:'w', keycode:87, source: 'break1'},
+  {key:'e', keycode:69, source: 'kick2'},
+  {key:'a', keycode:65, source: 'snare1'},
+  {key:'s', keycode:83, source: 'hat1'},
+  {key:'d', keycode:68, source: 'perc1'},
+  {key:'z', keycode:90, source: 'snare2'},
+  {key:'x', keycode:88, source: 'hat2'},
+  {key:'c', keycode:67, source: 'perc2'},
+]
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App drums={data} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
